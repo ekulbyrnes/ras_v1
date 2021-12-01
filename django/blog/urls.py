c86b3fd9743ff2788257b6ff428a75a4b8fe5081
ekulbyrnes <luke.e.byrnes@gmail.com>
2021-12-01 19:51:12 +1100

    rename page

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home', views.PageList.as_view(), name='home'),
+    path('', views.PageList.as_view(), name='home'),
     path('<slug:slug>/', views.PageDetail.as_view(), name='page_detail'),
 ]
